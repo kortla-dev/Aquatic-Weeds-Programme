f2dc8a6997d6bb05946a7f571ab4c25a199695ed
EvNat-44 <168890892+EvNat-44@users.noreply.github.com>
2024-08-20 13:49:11 +0200

    Update app.py
    Input Validation and Error Messages was added.

--- app.py
+++ app.py
@@ -34,43 +34,38 @@ fig_margins = {"t": 0, "b": 0, "l": 0, "r": 0}
 
 # NOTE: we use the dbc.Row and dbc.Col to support mobile and desktop
 # see https://dash-bootstrap-components.opensource.faculty.ai/docs/components/layout/ for more information
+# App layout with Bootstrap components
 app.layout = dbc.Container(
     id="core-container",
     children=[
-        # div wrapper that will be used to display invalid input messages
-        dbc.Row(
-            dbc.Col(
-                html.Div(
-                    id="inpt-val-msg-wrapper", children="Invalid input messages go here"
-                )
-            )
-        ),
-        # container for the graph and user input
         dbc.Row(
             id="graph-input-container",
             children=[
-                # This contains the graph
                 dbc.Col(
                     children=[dcc.Graph(id="graph-content")],
                 ),
-                # This contains the input fields and submition button
                 dbc.Col(
                     [
-                        # TODO: add tooltips to the inputs
                         dbc.InputGroup(
                             children=[
-                                dbc.InputGroupText("input 1"),
+                                dbc.InputGroupText("Input 1"),
                                 dbc.Input(id="input-1", type="text"),
                             ],
                         ),
+                        # New Code: Error message for Input 1
+                        html.Div(id="input1-val-msg", style={"color": "red"}),
+
                         dbc.InputGroup(
                             children=[
-                                dbc.InputGroupText("input 2"),
+                                dbc.InputGroupText("Input 2"),
                                 dbc.Input(id="input-2", type="text"),
                             ]
                         ),
+                        # New Code: Error message for Input 2
+                        html.Div(id="input2-val-msg", style={"color": "red"}),
+
                         dbc.InputGroup(
-                            children=[dbc.Button(id="button123", children="hello?")],
+                            children=[dbc.Button(id="button123", children="Submit")],
                         ),
                     ]
                 ),
@@ -80,41 +75,40 @@ app.layout = dbc.Container(
     fluid=True,
 )
 
-
+# Callback to update graph and display validation messages
 @callback(
     Output("graph-content", "figure"),
+    # New Code: Additional Outputs for validation messages
+    Output("input1-val-msg", "children"),
+    Output("input2-val-msg", "children"),
     [Input("button123", "n_clicks")],
-    [
-        State("input-1", "value"),
-        State("input-2", "value"),
-        State("inpt-val-msg-wrapper", "children"),
-    ],
+    [State("input-1", "value"), State("input-2", "value")]
 )
-def get_prediction(
-    n_clicks: Optional[int], input1, input2, val_msg: Optional[str]
-) -> go.Figure:
-    """tbd"""
+def get_prediction(n_clicks: Optional[int], input1, input2):
+    # New Code: Variables to hold validation messages
+    input1_msg, input2_msg = "", ""
 
-    # TODO: load a empty graph of the dam
-    # loads a default graph
-    if n_clicks is None:
-        fig = px.line(df[df.country == "Canada"], x="year", y="pop")
-        fig.update_layout(margin=fig_margins)
+    # Validate input1
+    if not input1 or not input1.strip():
+        input1_msg = "Input 1 is required."
 
-        return fig
+    # Validate input2
+    if not input2 or not input2.isdigit():
+        input2_msg = "Input 2 must be a valid number."
+    elif not (0 <= int(input2) <= 100):
+        input2_msg = "Input 2 must be between 0 and 100."
 
-    # HACK: i don't know if this is a good way to do it
-    # if there is a invalid input message the graph will not be updated
-    if val_msg:
-        raise PreventUpdate
+    # If there are validation errors, return them without updating the graph
+    if input1_msg or input2_msg:
+        # New Code: Returning validation messages instead of updating the graph
+        return dash.no_update, input1_msg, input2_msg
 
-    # TODO: call request a prediction from the Coordinator
+    # If all validations pass, update the graph
     fig = px.line(df[df.country == "Germany"], x="year", y="pop")
     fig.update_layout(margin=fig_margins)
 
-    return fig
-
+    # New Code: Clear validation messages upon successful validation
+    return fig, "", ""
 
 if __name__ == "__main__":
-    # NOTE: debug=True should not be sent to production
     app.run(debug=True)
